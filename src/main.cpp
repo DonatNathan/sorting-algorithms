2ea0c68b7529aa08f2f90782593f0f0f89d7b835
DonatNathan <nathan.donat-filliod@epitech.eu>
2025-02-12 10:16:59 -0800

    [ADD] - Colors and chrono

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <chrono>
+#include "colors.hpp"
 #include "utils.cpp"
 #include "algorithms/insertionSort.cpp"
 
@@ -6,12 +8,15 @@ int main(int argc, char **argv)
 {
     std::string fileContent = getUnsortedList(argv[1]);
     std::vector<int> unsortedList = stringToVector(fileContent);
+    std::vector<int> sortedList;
 
-    displayVector(unsortedList);
+    auto start = std::chrono::high_resolution_clock::now();
+    sortedList = insertionSort(unsortedList);
+    auto stop = std::chrono::high_resolution_clock::now();
 
-    unsortedList = insertionSort(unsortedList);
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
 
-    displayVector(unsortedList);
+    std::cout << BOLDGREEN << "List sorted in " << duration.count() << " microseconds" << std::endl;
 
     return 0;
 }
